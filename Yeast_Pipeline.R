4a14757eac042b8befde36c99b8231dcf82850ad
tailleferM <mylestaillefer@gmail.com>
2025-10-30 09:25:01 -0700

    added annotation command

--- Yeast_Pipeline.R
+++ Yeast_Pipeline.R
@@ -30,12 +30,27 @@ Mean coverage:	30
 
 #Funannotate
 
-#masking (need to do before annotation)
+#step 1 masking (need to do before annotation)
 docker run --rm -v "$PWD":/data -w /data --platform linux/amd64 nextgenusfs/funannotate funannotate mask -i trimmed_assembly.fasta -o tr_masked_assembly.fasta  
 
-#annotation -> prediction with funannotate
+#step 2 prediction 
 docker run --rm -v "$PWD":/data -w /data --platform linux/amd64 nextgenusfs/funannotate funannotate predict -i tr_masked_assembly.fasta -o /data/output -s 'Wickerhamomyces anomalus'
 
+#step 3 annotation
+docker run --rm -v "$PWD":/data -w /data --platform linux/amd64 nextgenusfs/funannotate funannotate annotate -i /data/output_attempt_1 -o /data/annotation_output
+
+try giving it rna data -> reference genome 
+align the reference rna seqs against my assembly -> use for funannotate
+
+
+
+
+
+#reading my funannotate gff file output 
+gffFile <- readLines('/Users/mtaillefer00/Documents/BIOL_4315_PROJECT/data/funannotate_stuff/output/predict_results/Wickerhamomyces_anomalus.gff3')
+
+library(stringr)
+products <- str_subset(gffFile, 'product=')
 
 #busco
 docker run --rm \
@@ -44,5 +59,8 @@ docker run --rm \
 ezlabgva/busco:v6.0.0_cv1 \
 busco -i assembly.fasta -o busco_output -l saccharomycetaceae_odb12 -m genome -c 8
 
-#
+library(blastinR)
+make_blast_db(infile = '/Users/mtaillefer00/Documents/BIOL_4315_PROJECT/data/funannotate_stuff/output/predict_results/Wickerhamomyces_anomalus.proteins.fa', dbtype = 'prot')
+
+
 
